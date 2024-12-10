330db0b5e9e1e80df11a9ca6b72cefbc58b81ee6
FLCKE <louisfrancisco229@gmail.com>
2024-12-10 14:50:43 +0100

    add candidatListView to the project

--- recrutement_app/src/app/candidatListView/page.js
+++ recrutement_app/src/app/candidatListView/page.js
@@ -1,9 +1,46 @@
-import React from 'react'
+"use client"
+import {React,useEffect, useState} from 'react'
+import { Avatar, List,Spin } from 'antd';
+import { GeneratorOfFormValues } from '../utilities/data';
+
+
+
+
+
+
+
 
 function page() {
+    const [formValuesArray, setFormValuesArray] = useState([]);
+
+    useEffect(() => {
+        const values = GeneratorOfFormValues();
+        setFormValuesArray(values);
+    }, []);
+    if(formValuesArray.length==0){
+        return (
+            <div className="text-center ">
+                <Spin tip="Loading" size="large" ></Spin>
+            </div>
+        )
+    }
+   
   return (
-    <div>page</div>
+      <List
+          itemLayout="horizontal"
+          dataSource={formValuesArray}
+          renderItem={(item) => (
+              <List.Item>
+                  <List.Item.Meta
+                      avatar={<Avatar src={item.personalInformation.cvFile} />}
+                      title={<a href="">{item.personalInformation.firstName} {item.personalInformation.lastName}</a>}
+                      description={item.personalInformation.desc}
+                      
+                      />
+              </List.Item>
+          )}
+      />
   )
 }
 
-export default page
+export default page;
---
+++ recrutement_app/src/app/utilities/data.js
@@ -0,0 +1,29 @@
+import { faker } from '@faker-js/faker';
+
+const generateFormValues = () => ({
+    personalInformation: {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        desc: faker.person.bio(),
+        phoneNumber: faker.phone.number('##########'), // Numéro de téléphone
+        birthday: faker.date.birthdate({ min: 18, max: 65, mode: 'age' }).toISOString().split('T')[0],
+    },
+    experience: Array.from({ length: faker.number.int({ min: 1, max: 5 }) }, () => ({
+        jobTitle: faker.name.jobTitle(),
+        businessOrClient: faker.company.name(),
+        location: faker.address.city(),
+        typeOfContract: faker.helpers.arrayElement(["Stage", "Alternance", "CDI", "CDD"]),
+        startDay: faker.date.past(5).toISOString().split('T')[0],
+        endDay: faker.date.future(1).toISOString().split('T')[0],
+        description: faker.lorem.sentences(2),
+    })),
+    skills: Array.from({ length: faker.number.int({ min: 1, max: 5 }) }, () => ({
+        skillName: faker.hacker.ingverb(),
+        skillLevel: faker.helpers.arrayElement(["Débutant", "Intermédiaire", "Pro"]),
+    })),
+    cvFile: faker.system.fileName('pdf'),
+});
+export function GeneratorOfFormValues(){
+    return Array.from({ length: 10 }, generateFormValues)
+}
